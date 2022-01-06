bd21afe3d02215569e3ba14d7be3df1071b6b207
Inhyeeee <51586135+inhye-world@users.noreply.github.com>
2022-01-06 17:57:23 +0900

    scroll 이동 js

--- main.js
+++ main.js
@@ -14,4 +14,19 @@ window.addEventListener("scroll", () =>{
         navbar.classList.remove("navbar__color");
     }
     
-})
+});
+
+/* handle scrolling when tapping on th navbar menu */
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
